d00030c26b9e7d1530f2df990fa9bab81ff46fc5
Jere <101744771+JerbsH@users.noreply.github.com>
2023-02-21 13:04:45 +0200

    mainView buttons working. Can move from first fragment to select party and show everyone fragments

--- app/src/main/java/com/example/membersofparliament_project/mainView.kt
+++ app/src/main/java/com/example/membersofparliament_project/mainView.kt
@@ -5,55 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.membersofparliament_project.databinding.FragmentMainViewBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [mainView.newInstance] factory method to
- * create an instance of this fragment.
- */
 class mainView : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var _binding: FragmentMainViewBinding
+    private val binding get() = _binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentMainViewBinding.inflate(layoutInflater)
+        binding.btnEveryone.setOnClickListener {
+            findNavController().navigate(R.id.action_mainView_to_showEveryone)
+        }
+        binding.btnParty.setOnClickListener {
+            findNavController().navigate(R.id.action_mainView_to_selectParty)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_view, container, false)
+        return binding.root //inflater.inflate(R.layout.fragment_main_view, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment mainView.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            mainView().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
